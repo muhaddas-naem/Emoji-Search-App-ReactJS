d64616977d9655e44495fded819ca0cefc407e4c
Muhaddas Naem <muhaddasnaem@gmail.com>
2024-10-06 10:57:02 +0600

    emoji-app-file-setup

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+
+      <div className='menu'>
+
+        <div className='menu-text'>
+
+          
+        <h1>Emoji Search</h1>
+        <p>A simple search with ReactJS</p>
+        <input type="text" name="" id="" placeholder='Search' />
+
+
+
+        </div>
+
+
+      <div className='container'>
+      
+      <div className='card'>
+        <p className='emo'>
+        😊
+        </p>
+
+        <p className='name'>
+          Cry
+        </p>
+
+      </div>
+
+      </div>
+
+      </div>
+      
+
+
+
+    </div>
+  );
+}
+
+export default App
